8592228028dd223f891ac82d0c149aa98a9b678f
Djeymisson <djeymisson@gmail.com>
2022-04-07 01:04:49 -0300

    feat(projects): cash register challenge

---
+++ js-algorithms-data-structures-projects/CashRegister.js
@@ -0,0 +1,96 @@
+/* 
+
+//// TODO: try recursive approach
+
+Cash Register
+Design a cash register drawer function checkCashRegister() that accepts purchase price as the first argument (price), payment as the second argument (cash), and cash-in-drawer (cid) as the third argument.
+
+cid is a 2D array listing available currency.
+
+The checkCashRegister() function should always return an object with a status key and a change key.
+
+Return {status: "INSUFFICIENT_FUNDS", change: []} if cash-in-drawer is less than the change due, or if you cannot return the exact change.
+
+Return {status: "CLOSED", change: [...]} with cash-in-drawer as the value for the key change if it is equal to the change due.
+
+Otherwise, return {status: "OPEN", change: [...]}, with the change due in coins and bills, sorted in highest to lowest order, as the value of the change key.
+
+Test cases:
+    checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return an object.
+
+    checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["QUARTER", 0.5]]}.
+
+    checkCashRegister(3.26, 100, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]) should return {status: "OPEN", change: [["TWENTY", 60], ["TEN", 20], ["FIVE", 15], ["ONE", 1], ["QUARTER", 0.5], ["DIME", 0.2], ["PENNY", 0.04]]}.
+
+    checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+    checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "INSUFFICIENT_FUNDS", change: []}.
+
+    checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]) should return {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}.
+
+*/
+
+function checkCashRegister(price, cash, cid) {
+    const currencyValue = {
+        PENNY: 0.01,
+        NICKEL: 0.05,
+        DIME: 0.1,
+        QUARTER: 0.25,
+        ONE: 1,
+        FIVE: 5,
+        TEN: 10,
+        TWENTY: 20,
+        "ONE HUNDRED": 100,
+    };
+
+    const sumCurrencies = (bills) => bills.reduce((p, c) => p + c[1], 0);
+
+    const totalValueOfRequiredNumberOfUnit = (unit) =>
+        Math.floor(
+            (changeDue - Math.floor(changeDue % currencyValue[unit])) /
+                currencyValue[unit]
+        ) * currencyValue[unit];
+
+    const highestToLowestOrder = (unit) =>
+        Object.keys(currencyValue).indexOf(unit);
+
+    const cidCopy = [...cid].sort(
+        (a, b) => highestToLowestOrder(b[0]) - highestToLowestOrder(a[0])
+    );
+
+    let changeDue = cash - price;
+    const change = cidCopy.reduce((result, c) => {
+        const total = totalValueOfRequiredNumberOfUnit(c[0]);
+        if (total > 0) {
+            changeDue =
+                Math.round(
+                    (changeDue + Number.EPSILON) * 100 -
+                        (Math.min(c[1], total) + Number.EPSILON) * 100
+                ) / 100;
+            result.push([c[0], Math.min(c[1], total)]);
+        }
+        return result;
+    }, []);
+
+    if (changeDue > 0) return { status: "INSUFFICIENT_FUNDS", change: [] };
+
+    if (sumCurrencies(change) == sumCurrencies(cid))
+        return { status: "CLOSED", change: cid };
+
+    return { status: "OPEN", change };
+}
+
+console.log(
+    checkCashRegister(3.26, 100, [
+        ["PENNY", 1.01],
+        ["NICKEL", 2.05],
+        ["DIME", 3.1],
+        ["QUARTER", 4.25],
+        ["ONE", 90],
+        ["FIVE", 55],
+        ["TEN", 20],
+        ["TWENTY", 60],
+        ["ONE HUNDRED", 100],
+    ])
+);
+
